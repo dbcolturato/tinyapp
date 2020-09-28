96d4215904385c9f2b6d87a24f5b4746d9e01aef
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-09-28 23:04:08 +0000

    URL Shortening (part 1)

--- express_server.js
+++ express_server.js
@@ -4,11 +4,20 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+//convert the request body from a Buffer into string that we can read
+//It will then add the data to the req(request) object under the key body
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 //keeping track of all the URLs and their shortened forms
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,6 +35,15 @@ app.get("/urls", (req, res) => {
   const templatesVars = { urls: urlDatabase };
   res.render("urls_index", templatesVars);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
